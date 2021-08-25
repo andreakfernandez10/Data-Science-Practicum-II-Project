b5dddf20a2d6ea1c61ab2c9edbd814adf11a2d3b
andreakfernandez10 <60277706+andreakfernandez10@users.noreply.github.com>
2021-08-25 16:11:51 -0600

    Update Full Project.py

--- Full Project.py
+++ Full Project.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[255]:
-
-
 import numpy as np
 import pandas as pd
 import seaborn as sns
@@ -31,22 +28,13 @@ from importlib import reload
 print ("pandas version ",pd.__version__)
 
 
-# In[3]:
-
-
 # Load ucldata into python
 ucldata= pd.read_csv(r'/Users/andrea/Downloads/ucl-soccer-1955-2019.csv')
-
-
-# In[4]:
 
 
 # make sure data was uploaded and is correct
 ucldata.head()
 ucldata.tail()
-
-
-# In[430]:
 
 
 # Download champs data 
@@ -55,17 +43,10 @@ champs.head()
 champs.tail()
 
 
-# In[431]:
-
-
 # assess champs data 
 champs.shape
 champs.info()
 champs.isnull().sum()
-
-
-# In[432]:
-
 
 # Data cleaning
 champs.isnull()
@@ -80,67 +61,36 @@ champs["tiewinner"].fillna("No tie", inplace = True)
 # Confirming the data does not have NA values
 champs.isnull().sum()
 
-
-# In[433]:
-
-
 # Check type for each column after cleaning
 champs.info()
-
-
-# In[113]:
-
 
 # Check missing values for ucldata
 ucldata.isnull()
 ucldata.isnull().sum()
 
-
-# In[10]:
-
-
 # Exploratory Data Analysis UCL Data
 ucldata.info()
 ucldata.shape
-
-
-# In[11]:
-
 
 # EDA cont.
 ucldata.describe()
 ucldata['club'].value_counts().head(5)
 
 
-# In[12]:
-
-
 # Top 5 nations that teams come from
 ucldata['nation'].value_counts().head(5)
-
-
-# In[13]:
 
 
 # Top 10 teams in the final 
 ucldata['club'].value_counts().head(10)
 
 
-# In[14]:
-
-
 # EDA champs data
 champs['home'].value_counts().head(10)
 
 
-# In[15]:
-
-
 #Top 10 visiting teams
 champs['visitor'].value_counts().head(10)
-
-
-# In[244]:
 
 
 # Top 5 score outcomes for all games
@@ -150,9 +100,6 @@ plt.title('Top 5 Score Outcomes')
 plt.show()
 
 
-# In[237]:
-
-
 # Countries represented in tournament since 1955
 champs.hcountry.value_counts(normalize=True)
 champs.hcountry.value_counts(normalize=True).plot.pie()
@@ -160,39 +107,21 @@ champs.hcountry.value_counts(normalize=True).plot.pie()
 plt.show()
 
 
-# In[18]:
-
-
 # Taking a look at the aggregate values for home teams
 champs.FTagg_home.describe()
 
 
-# In[19]:
-
-
 # Aggregate values for visitor teams which has a lower mean
 champs.FTagg_visitor.describe()
-
-
-# In[32]:
-
 
 # scatter plt of aggregate goals for home and visitor
 plt.scatter(champs.FTagg_home,champs.FTagg_visitor)
 plt.show()
 
 
-# In[37]:
-
-
 from pandas import DataFrame
 from numpy import array
 from sklearn.ensemble import RandomForestClassifier
-
-
-# In[455]:
-
-
 import matplotlib as mp
 from matplotlib import colors as mcolors
 
@@ -207,53 +136,22 @@ ax.set_xticklabels(ax.get_xticklabels(), rotation=45, horizontalalignment='right
 plt.show()
 
 
-# In[434]:
-
-
 # In order to run the model these columns needed to be deleted to not disrupt it. They are object types like data
 champs = champs.drop(champs.columns[[0,1,2,3,9,20]], axis=1)
-
-
-# In[462]:
 
 
 # Checking to see the new dataset 
 champs.head()
 
-
-# In[463]:
-
-
-### Preparing Data
-
-
-# In[199]:
-
-
-
+## Preparing Data
 
 x_all = champs.drop(['FT', 'home', 'visitor', 'hcountry', 'vcountry'], 1)
 y_all = champs['FT']
 
-
-# In[200]:
-
-
 from sklearn.preprocessing import scale
-
-
-# In[201]:
-
-
 cols=[['hgoal', 'vgoal', 'totvgoal', 'tothgoal', 'totagg_home', 'totagg_visitor']]
 for col in cols:
     x_all[col] = scale(x_all[col])
-
-
-# In[202]:
-
-
-
 
 def preprocess_features(X):
     '''Preprocesses the soccer data and converts catagorical variables into dummy variables.'''
@@ -274,29 +172,16 @@ def preprocess_features(X):
 x_all = preprocess_features(x_all)
 print ("Processed feature columns ({} total features):\n{}".format(len(x_all.columns), list(x_all.columns)))
 
-
-# In[203]:
-
-
 print ("\nFeature values:")
 display(x_all.head())
-
-
-# In[351]:
-
 
 from sklearn.model_selection import train_test_split
 
 x_train, x_test, y_train, y_test = train_test_split(x_all, y_all, test_size = 100, random_state = 2)
 
 
-# In[352]:
-
-
 from time import time
-
 from sklearn.metrics import f1_score
-
 def train_classifier(clf, x_train, y_train):
     ''' Fits a classifier to the training data.'''
     
@@ -330,9 +215,6 @@ def train_predict(clf, x_train, y_train, x_test, y_test):
     print ("F1 score and accuracy score for test set: {:.4f}, {:.4f}.".format(f1, acc))
 
 
-# In[353]:
-
-
 import xgboost as xgb
 
 clf_A = LogisticRegression(random_state = 42, max_iter=2000)
@@ -346,9 +228,6 @@ train_predict(clf_B, x_train, y_train, x_test, y_test)
 print('')
 train_predict(clf_C, x_train, y_train, x_test, y_test)
 print('')
-
-
-# In[212]:
 
 
 from sklearn.model_selection import RandomizedSearchCV, GridSearchCV, train_test_split
@@ -384,20 +263,8 @@ f1, acc = predict_labels(clf, x_test, y_test)
 print ("F1 score and accuracy score for test set: {:.4f} , {:.4f}.".format(f1, acc))
 
 
-# In[214]:
-
-
 prediction=clf.predict(x_train)
-
-
-# In[215]:
-
-
 print(prediction)
-
-
-# In[216]:
-
 
 from sklearn.model_selection import RandomizedSearchCV, GridSearchCV, train_test_split
 from sklearn.metrics import make_scorer
@@ -430,10 +297,6 @@ print (clf)
 f1, acc = predict_labels(clf, x_train, y_train)
 print ("F1 score and accuracy score for trainig set: {:.4f} , {:.4f}.".format(f1 , acc))
 
-
-# In[454]:
-
-
 # Save Model using joblib
 import joblib
 
@@ -445,25 +308,14 @@ loaded_model = joblib.load(filename)
 result = loaded_model.score(x_test, y_test)
 print(result)
 
-
-# In[428]:
-
-
 import sys
 import numpy
 numpy.set_printoptions(threshold=sys.maxsize)
 
 numpy.vstack((y_train, yhat_binary)).T
 
-
-# In[441]:
-
-
 yhat = clf_A.predict(x_test)
 yhat
-
-
-# In[442]:
 
 
 home = champs.loc[ :99, 'home']
@@ -476,16 +328,10 @@ print(x_train.shape)
 print(y_train.shape)
 
 
-# In[443]:
-
-
 from sklearn import metrics
 
 yhat_binary =clf_A.predict(x_train)
 print("Accuracy:",metrics.accuracy_score(y_train, yhat_binary))
-
-
-# In[444]:
 
 
 import sys
@@ -495,41 +341,17 @@ yhat_binary = clf_A.predict(x_train)
 print('Accuracy:', metrics.accuracy_score(y_train, yhat_binary))
 
 
-# In[445]:
-
-
 df = pd.DataFrame({'home': home, 'Predictions':yhat, 'visitor': visitor})
 df
 
-
-# In[446]:
 
 
 yhat_prob = clf_A.predict(x_test)
 yhat_prob
 
 
-# In[448]:
-
-
 df2 = pd.DataFrame ({'home': home, 'Prediction': yhat_prob[:,1], 'visitor': visitor})
-
-
-# In[375]:
-
-
-champs.columns
-
-
-# In[376]:
 
 
 import warnings
 warnings.filterwarnings('ignore')
-
-
-# In[ ]:
-
-
-
-
